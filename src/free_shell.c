7720e1216931dd357a6a91e6bcc4fdd07517b1a3
Nguyen Tam Anh Bui <tbui@Nguyens-MacBook-Pro.local>
2017-08-08 20:30:44 +0200

    [UPD] Freed the env, but still need to test thoroughly and see if nothing's been missed

--- src/free_shell.c
+++ src/free_shell.c
@@ -1,19 +1,42 @@
 #include "../includes/ft_sh.h"
 
 /*
-** Takes in the shell and free it and its environment
+** Takes in the shell's environment and frees it
+*/
+void        free_env(t_env *env)
+{
+    t_env *tmp;
+    
+    tmp = NULL;
+    tmp = env;
+    while (tmp->next)
+        tmp = tmp->next;
+
+    if (tmp->prev)
+        tmp = tmp->prev;
+    while (tmp->prev)
+    {
+       tmp = tmp->prev;
+       free(tmp->next->name);
+       tmp->next->name = NULL;
+       free(tmp->next->value);
+       tmp->next->value = NULL;
+       free(tmp->next);
+       tmp->next = NULL;
+    }
+}
+
+/*
+** Takes in the shell and frees it
 */
 void        free_shell(t_shell *shell)
 {
-    t_env *tmp_env;
-
-    tmp_env = shell->env;
     if (!shell)
         fatal("No shell to be freed in (free_shell)");
+ 
     // freeing the environment
-    while (tmp_env->next)
-    {
-        ft_putendl(tmp_env->name);
-        tmp_env = tmp_env->next;
-    }
+    free_env(shell->env);
+ 
+    // TODO More might be implemented later as we add more onto the shell
+
 }
