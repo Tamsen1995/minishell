ab6be276aadebb6e912d579edea9123f91a5a007
Tamanh BUI <tbui@e1r7p6.42.fr>
2017-09-29 12:29:47 +0200

    [NORM]

--- src/init_shell.c
+++ src/init_shell.c
@@ -1,19 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_shell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/29 11:47:45 by tbui              #+#    #+#             */
+/*   Updated: 2017/09/29 11:51:00 by tbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/ft_sh.h"
 
-// initiaing the shell and the data required for it
-t_shell     *init_shell(int ac, char **av, char **envv)
-{
-    t_shell *shell;
+/*
+** initiaing the shell and the data required for it
+*/
 
-    shell = NULL;
-    av = NULL;
-    ac = 0;
-    if (!(shell = (t_shell *)malloc(sizeof(t_shell))))
-        fatal("Couldn't allocate shell in init_shell");
-    shell->env = init_env(envv);
-    shell->argc = 0;
-    shell->args = NULL;
-    shell->path_var = NULL;
-    shell->bin_dir = NULL;
-    return (shell); // TESTING
+t_shell				*init_shell(int ac, char **av, char **envv)
+{
+	t_shell *shell;
+
+	shell = NULL;
+	av = NULL;
+	ac = 0;
+	if (!(shell = (t_shell *)malloc(sizeof(t_shell))))
+		fatal("Couldn't allocate shell in init_shell");
+	shell->env = init_env(envv);
+	shell->argc = 0;
+	shell->args = NULL;
+	shell->path_var = NULL;
+	shell->bin_dir = NULL;
+	return (shell);
 }
