b31621e29304577815fb4c4333ac5b298b47c18f
Tamanh BUI <tbui@e1r7p6.42.fr>
2017-09-29 11:43:04 +0200

    [NORM]

--- src/sh_launch.c
+++ src/sh_launch.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sh_launch.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/29 11:41:33 by tbui              #+#    #+#             */
+/*   Updated: 2017/09/29 11:41:37 by tbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/ft_sh.h"
 
 /*
@@ -6,43 +18,42 @@
 ** to make a path to be executed by execve
 */
 
-char        *make_bin_cmd(t_shell *shell)
+char			*make_bin_cmd(t_shell *shell)
 {
-    char *command;
-    int len_cmd;
+	char		*command;
+	int			len_cmd;
 
-    command = NULL;
-    len_cmd = 0;
-    if (!shell || !shell->bin_dir || !shell->args[0])
-        fatal("Error in (make_bin_cmd)");
-    len_cmd = ft_strlen(shell->bin_dir) + 1;
-    len_cmd = len_cmd + ft_strlen(shell->args[0]);
-    command = ft_strnew(len_cmd);
-    command = ft_strcat(command, shell->bin_dir);
-    command = ft_strcat(command, "/");
-    command = ft_strcat(command, shell->args[0]);
-    return (command);
+	command = NULL;
+	len_cmd = 0;
+	if (!shell || !shell->bin_dir || !shell->args[0])
+		fatal("Error in (make_bin_cmd)");
+	len_cmd = ft_strlen(shell->bin_dir) + 1;
+	len_cmd = len_cmd + ft_strlen(shell->args[0]);
+	command = ft_strnew(len_cmd);
+	command = ft_strcat(command, shell->bin_dir);
+	command = ft_strcat(command, "/");
+	command = ft_strcat(command, shell->args[0]);
+	return (command);
 }
 
-
 /*
-** simply a function which exists if the typed in 
+** simply a function which exists if the typed in
 ** command hasn't
 ** been found
 */
 
-void        cmd_not_found(t_shell *shell)
+void			cmd_not_found(t_shell *shell)
 {
-    char *command;
+	char *command;
 
-    command = NULL;
-    if (!shell || !shell->args[0])
-        fatal("Error in (cmd_not_found");
-    ft_putstr("tamshell: command not found: ");
-    ft_putendl(shell->args[0]);
-    free_twod_arr(shell->args);
-    free_shell(shell);
-    exit(-1);
+	command = NULL;
+	if (!shell || !shell->args[0])
+		fatal("Error in (cmd_not_found");
+	ft_putstr("tamshell: command not found: ");
+	ft_putendl(shell->args[0]);
+	free_twod_arr(shell->args);
+	free_shell(shell);
+	exit(-1);
 }
 
 /*
@@ -50,37 +61,32 @@ void        cmd_not_found(t_shell *shell)
 ** the child process
 */
 
-int         sh_launch(char **envv, t_shell *shell)
+int				sh_launch(char **envv, t_shell *shell)
 {
-    pid_t pid;
-    pid_t wpid;
-    int status;
-    char *command;
+	pid_t		pid;
+	pid_t		wpid;
+	int			status;
+	char		*command;
 
-    // fork program and save the return
-    command = NULL;
-    pid = fork();
-    if (check_bin_cmd(shell) == TRUE)
-        command = make_bin_cmd(shell); // TODO free the command
-    else if (check_bin_path(shell) == TRUE)
-        command = ft_strdup(shell->args[0]); // TODO free the command
-    if (pid == 0)
-    {
-        if (execve(command, shell->args, envv) == -1)
-            cmd_not_found(shell);
-    }
-    else if (pid < 0)
-        fatal("sh_launch ERR:002");
-    else
-    {
-        // this condition is meant for the parent process
-        // it waits for the status to end, either by exit
-        // or signal et cetera
-        wpid = waitpid(pid, &status, WUNTRACED);
-        while (!WIFEXITED(status) && !WIFSIGNALED(status))
-            wpid = waitpid(pid, &status, WUNTRACED);
-    }
-    ft_strfree(command);
-    return (1);
+	command = NULL;
+	pid = fork();
+	if (check_bin_cmd(shell) == TRUE)
+		command = make_bin_cmd(shell);
+	else if (check_bin_path(shell) == TRUE)
+		command = ft_strdup(shell->args[0]);
+	if (pid == 0)
+	{
+		if (execve(command, shell->args, envv) == -1)
+			cmd_not_found(shell);
+	}
+	else if (pid < 0)
+		fatal("sh_launch ERR:002");
+	else
+	{
+		wpid = waitpid(pid, &status, WUNTRACED);
+		while (!WIFEXITED(status) && !WIFSIGNALED(status))
+			wpid = waitpid(pid, &status, WUNTRACED);
+	}
+	ft_strfree(command);
+	return (1);
 }
-
