85bc5d401e306a92568ef725c85db23127d01c72
Tamanh BUI <tbui@e1r7p6.42.fr>
2017-09-29 16:37:25 +0200

    [NORM]

--- src/sh_execute.c
+++ src/sh_execute.c
@@ -1,14 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sh_execute.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/29 16:32:46 by tbui              #+#    #+#             */
+/*   Updated: 2017/09/29 16:34:02 by tbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/ft_sh.h"
 
+/*
+** function that will either start a process or a builtin
+*/
 
-// function that will either start a process or a builtin
-int sh_execute(char **envv, t_shell *shell)
+int				sh_execute(char **envv, t_shell *shell)
 {
-    // There was an empty command
-    if (!shell->args || !shell->args[0])
-        return (1);
-    if (check_builtins(shell->args[0]) == TRUE || \
-        check_builtin_path(shell) == TRUE) 
-        return (exec_builtin(shell));
-    return (sh_launch(envv, shell));
+	if (!shell->args || !shell->args[0])
+		return (1);
+	if (check_builtins(shell->args[0]) == TRUE || \
+			check_builtin_path(shell) == TRUE)
+		return (exec_builtin(shell));
+	return (sh_launch(envv, shell));
 }
