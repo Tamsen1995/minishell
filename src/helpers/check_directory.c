5fbd4f09dab434f71bd24a482694dc0555899bb9
Tamanh BUI <tbui@e1r7p6.42.fr>
2017-09-29 17:55:05 +0200

    [NORM]

--- src/helpers/check_directory.c
+++ src/helpers/check_directory.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_directory.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/29 17:50:02 by tbui              #+#    #+#             */
+/*   Updated: 2017/09/29 17:51:37 by tbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/ft_sh.h"
 
 /*
@@ -6,27 +18,25 @@
 ** sought after filename
 */
 
-T_BOOL        check_directory(char *dir_path, char *file)
+T_BOOL				check_directory(char *dir_path, char *file)
 {
-    DIR             *dir;
-    struct dirent   *ent;
-    //char *bin;
+	DIR				*dir;
+	struct dirent	*ent;
 
-    dir = NULL;
-    ent = NULL;
-    if (!(dir = opendir(dir_path)))
-        fatal("Could not open directory in (check_directory)");
-    if (!dir_path || !file)
-        fatal("Error in (check_directory)"); // remove later
-    while ((ent = readdir(dir)))
-    { 
-        // iterate over all binaries in folder to look for sought after binary
-        if (ft_strcmp(ent->d_name, file) == 0)
-        {
-            closedir(dir);
-            return (TRUE);
-        }
-    }
-    closedir(dir);
-    return (FALSE);
+	dir = NULL;
+	ent = NULL;
+	if (!(dir = opendir(dir_path)))
+		fatal("Could not open directory in (check_directory)");
+	if (!dir_path || !file)
+		fatal("Error in (check_directory)");
+	while ((ent = readdir(dir)))
+	{
+		if (ft_strcmp(ent->d_name, file) == 0)
+		{
+			closedir(dir);
+			return (TRUE);
+		}
+	}
+	closedir(dir);
+	return (FALSE);
 }
