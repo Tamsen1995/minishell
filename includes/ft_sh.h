840ecde550e8cf72b7cc18daf81d3ed855bfa943
Tamanh BUI <tbui@e2r10p4.42.fr>
2017-12-08 19:23:33 +0100

    [NORM]

--- includes/ft_sh.h
+++ includes/ft_sh.h
@@ -1,25 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sh.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/08 18:54:59 by tbui              #+#    #+#             */
+/*   Updated: 2017/12/08 19:23:05 by tbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_SH_H
 # define FT_SH_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/param.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include "../includes/libft.h"
-#include "../includes/ft_ls.h"
-
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <sys/param.h>
+# include <signal.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include "../includes/libft.h"
+# include "../includes/ft_ls.h"
 
 # define BUFF_SIZE 8
 # define BIN "/bin/"
-#define T_BOOL int
-#define TRUE 1
-#define FALSE 0
+# define T_BOOL int
+# define TRUE 1
+# define FALSE 0
 
-// This list determines the current state of the shell
 typedef struct		s_env
 {
 	struct s_env	*next;
@@ -30,35 +40,33 @@ typedef struct		s_env
 
 typedef struct		s_shell
 {
-	int				argc; // Amount of arguments passed into my shell
+	int				argc;
 	char			**args;
-	struct s_env	*env; // The environment variables
-	char			*path_var; // The value of PATH
-	char			*bin_dir; // the binary folder in which the sought after CURRENT command is
-	// Add more data variables later
+	struct s_env	*env;
+	char			*path_var;
+	char			*bin_dir;
 }					t_shell;
-
 
 T_BOOL				check_builtin_path(t_shell *shell);
 int					exec_builtin(t_shell *shell);
 T_BOOL				check_builtins(char *cmd);
 void				change_env_var(char **args, t_shell *shell);
 T_BOOL				check_bin_path(t_shell *shell);
-T_BOOL    		    check_directory(char *dir_path, char *file);
+T_BOOL				check_directory(char *dir_path, char *file);
 void				get_path_var(t_shell *shell);
-void    		    free_shell(t_shell *shell);
+void				free_shell(t_shell *shell);
 int					sh_unsetenv(char **args, t_shell *shell);
 void				ft_putenv(t_env **begin_list, char *name, char *value);
-T_BOOL  		    check_bin_cmd(t_shell *shell);
-int      			sh_env(t_shell *shell);
-int   				sh_setenv(char **args, t_shell *shell);
-t_env   		    *init_env(char **envv);
+T_BOOL				check_bin_cmd(t_shell *shell);
+int					sh_env(t_shell *shell);
+int					sh_setenv(char **args, t_shell *shell);
+t_env				*init_env(char **envv);
 int					get_next_line(int const fd, char **line);
 int					sh_execute(char **envv, t_shell *shell);
 int					sh_launch(char **envv, t_shell *shell);
 int					sh_cd(char **args, t_shell *shell);
 int					sh_exit(void);
 int					sh_echo(char **args);
-t_shell  		   *init_shell(int ac, char **av, char **envv);
+t_shell				*init_shell(int ac, char **av, char **envv);
 
 #endif
