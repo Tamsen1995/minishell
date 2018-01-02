98ef6c7f6e23c9d012d13ab8fe3191b5cda41bd0
Tamanh BUI <tbui@e1r1p8.42.fr>
2018-01-02 17:59:13 +0100

    [UPD] Made the execution of several commands with the use of ; as a seperator possible.

--- src/helpers/store_commands.c
+++ src/helpers/store_commands.c
@@ -1,45 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   store_commands.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/02 17:53:02 by tbui              #+#    #+#             */
+/*   Updated: 2018/01/02 17:58:09 by tbui             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/ft_sh.h"
 
 /*
 ** allocates a new environment variables list element
 */
 
-t_cmds      *ft_new_cmd(char *command)
+t_cmds		*ft_new_cmd(char *command)
 {
-    t_cmds *new;
+	t_cmds *new;
 
-    if (!(new = (t_cmds *)malloc(sizeof(t_cmds))))
-        fatal("Error: could not allocate env var list elem in ft_new_env");
-    new->next = NULL;
-    new->prev = NULL;
-    new->args = NULL;
-    new->args = ft_strsplit(command, ' ');
-    return (new);
+	if (!(new = (t_cmds *)malloc(sizeof(t_cmds))))
+		fatal("Error: could not allocate env var list elem in ft_new_env");
+	new->next = NULL;
+	new->prev = NULL;
+	new->args = NULL;
+	new->args = ft_strsplit(command, ' ');
+	return (new);
 }
 
-/* 
+/*
 ** iterates to the end of the cmd list
 ** and then adds the value to it
 */
 
 void		ft_add_cmd(t_cmds **begin_list, char *command)
 {
-    t_cmds *tmp;
+	t_cmds *tmp;
 
-    tmp = NULL;
+	tmp = NULL;
 	if (!*begin_list)
 	{
 		*begin_list = ft_new_cmd(command);
 		return ;
 	}
-    else
-    {
-        tmp = *begin_list;
-        while (tmp->next)
-            tmp = tmp->next;
-        tmp->next = ft_new_cmd(command);
-        tmp->next->prev = tmp;
-    }
+	else
+	{
+		tmp = *begin_list;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = ft_new_cmd(command);
+		tmp->next->prev = tmp;
+	}
 }
 
 /*
@@ -50,28 +62,28 @@ void		ft_add_cmd(t_cmds **begin_list, char *command)
 ** initial one
 */
 
-t_cmds      *store_commands(char *commands)
+t_cmds		*store_commands(char *commands)
 {
-    char    **cmds_arr; // an array to temporarily store the commands inside
-    t_cmds  *ret;
-    int     i;
+	char	**cmds_arr;
+	t_cmds	*ret;
+	int		i;
 
-    cmds_arr = NULL;
-    ret = NULL;
-    i = 0;
-    if (!commands)
-        return (NULL);
-    cmds_arr = ft_strsplit(commands, ';');
-    if (!cmds_arr)
-    {
-        ft_add_cmd(&ret, commands);
-        return (ret);
-    }
-    while (cmds_arr[i]) // while there are potential commands
-    {
-        ft_add_cmd(&ret, cmds_arr[i]);
-        i++;
-    }
-    free_twod_arr(cmds_arr);
-    return (ret);
+	cmds_arr = NULL;
+	ret = NULL;
+	i = 0;
+	if (!commands)
+		return (NULL);
+	cmds_arr = ft_strsplit(commands, ';');
+	if (!cmds_arr)
+	{
+		ft_add_cmd(&ret, commands);
+		return (ret);
+	}
+	while (cmds_arr[i])
+	{
+		ft_add_cmd(&ret, cmds_arr[i]);
+		i++;
+	}
+	free_twod_arr(cmds_arr);
+	return (ret);
 }
