5a3a4c7acc58b465a9565481378d49151303d8c7
Tamanh BUI <tbui@e3r5p1.42.fr>
2018-01-18 16:22:57 +0100

    [UPD] redid the cd function

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -13,45 +13,60 @@
 #include "../../includes/ft_sh.h"
 
 /*
-** changes the oldpwd
+** iterates to the pwd to be updated. if it exists then it updates it.
+** if not then it adds it to the list
 */
 
-void		change_oldpwd(char *cwd, t_shell *shell)
+void		put_pwd(t_shell *shell, char *pwd)
 {
-	t_env	*tmp_env;
+	char		current_dir[BUF_SIZE];
+	t_env		*tmp_env;
 
-	if (!cwd || !shell || !shell->env)
-		fatal("Error in change_oldpwd");
+
 	tmp_env = NULL;
+	ft_bzero(current_dir, BUF_SIZE);
+	if (getcwd(current_dir, sizeof(current_dir)) == NULL)
+		fatal("Error 2 in : (update_oldpwd)");
 	tmp_env = shell->env;
-	while (tmp_env && ft_strcmp("OLDPWD", tmp_env->name) != 0)
+	while (tmp_env->next && ft_strcmp(tmp_env->name, pwd) != 0)
 		tmp_env = tmp_env->next;
-	ft_strfree(tmp_env->value);
-	tmp_env->value = ft_strdup(cwd);
+	if (ft_strcmp(tmp_env->name, pwd) == 0)
+		change_env_var(pwd, current_dir, shell);
+	else
+		ft_putenv(&shell->env, pwd, current_dir);
 }
 
 /*
-** goes to the parent folder and
-** shows its path
-** always relative to the current path
+** simply a function to update the oldpwd OR the PWD
+** within the environment variable list
+** sets the oldpwd / owd to the current working directory
 */
 
-void		precede_nd_show(t_shell *shell)
+void		update_pwd(enum e_pwd pwd, t_shell *shell)
 {
-	char cwd[BUF_SIZE];
-	char *old_pwd;
+	if (!shell || !shell->env)
+		fatal("Error 1 in : (update_oldpwd)");
+	if (pwd == OLD)
+		put_pwd(shell, "OLDPWD"); // TODO : setenv the oldpwd
+	if (pwd == CUR)
+		put_pwd(shell, "PWD");
+}
 
-	old_pwd = NULL;
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
+
+/*
+** a safe wrapper around chdir
+*/
+
+void		safe_chdir(t_shell *shell, char *path)
+{
+	update_pwd(OLD, shell);
+	if (!path)
+		return ;
+	if (chdir(path) != 0 && ft_strcmp(path, "-") != 0)
 	{
-		ft_putendl(strerror(errno));
-		fatal("Expand buf size in (precede_nd_show)");
+		ft_putstr("My cd: No such file or directory: ");
+		ft_putendl(path);
 	}
-	old_pwd = get_env_var(shell, "OLDPWD");
-	chdir(old_pwd);
-	change_oldpwd(cwd, shell);
-	ft_putendl(old_pwd);
-	ft_strfree(old_pwd);
 }
 
 /*
@@ -60,21 +75,20 @@ void		precede_nd_show(t_shell *shell)
 
 int			sh_cd(char **args, t_shell *shell)
 {
+	char *oldpwd;
+
+	oldpwd = NULL;
+	if (!args || !shell)
+		fatal("Error in : (sh_cd)");
+	if (args[1] && ft_strcmp(args[1], "-") == 0)
+	{
+		oldpwd = get_env_var(shell, "OLDPWD");
+		safe_chdir(shell, oldpwd);
+	}
 	if (args[1] == NULL)
-	{
-		if (chdir(shell->home_path) != 0)
-			fatal("Error in (sh_cd)");
-	}
-	else if (ft_strcmp(args[1], "-") == 0)
-		precede_nd_show(shell);
+		safe_chdir(shell, shell->home_path);
 	else
-	{
-		if (chdir(args[1]) != 0)
-		{
-			ft_putstr("My cd: No such file or directory: ");
-			ft_putendl(args[1]);
-			return (1);
-		}
-	}
+		safe_chdir(shell, args[1]);
+	update_pwd(CUR, shell);
 	return (1);
 }
